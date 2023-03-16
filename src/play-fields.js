0306514c1c385fe1a2e7fef1411dc68df6d0a16a
Maoleshko <ranshe.rassveta@gmail.com>
2023-03-16 21:45:07 +0300

    4.3

--- src/play-fields.js
+++ src/play-fields.js
@@ -1,19 +1,210 @@
 function renderPlayBlock(container) {
+    const h1 = document.createElement('h1')
+    h1.textContent = '00:00'
+    h1.classList.add('timer')
+
     const resetButton = document.createElement('button')
     resetButton.textContent = 'Начать заново'
     resetButton.classList.add('button', 'btn-reset')
 
+
+    container.appendChild(h1)
+    container.appendChild(resetButton)
+    
+    
+}
+
+window.application.blocks['play'] = renderPlayBlock
+
+function renderCardListBlock(container) {
+    
     const cardListData = [
+        {
+            id: 'spades-6',
+            name: 'spades 6',
+            image: './img/6p.png',
+        },
+        {
+            id: 'spades-7',
+            name: 'spades 7',
+            image: './img/7p.png',
+        },
+        {
+            id: 'spades-8',
+            name: 'spades 8',
+            image: './img/8p.png',
+        },
+        {
+            id: 'spades-9',
+            name: 'spades 9',
+            image: './img/9p.png',
+        },
+        {
+            id: 'spades-10',
+            name: 'spades 10',
+            image: './img/10p.png',
+        },
+        {
+            id: 'spades-j',
+            name: 'spades j',
+            image: './img/jp.png',
+        },
+        {
+            id: 'spades-q',
+            name: 'spades q',
+            image: './img/qp.png',
+        },
+        {
+            id: 'spades-k',
+            name: 'spades k',
+            image: './img/kp.png',
+        },
+        {
+            id: 'spades-a',
+            name: 'spades a',
+            image: './img/ap.png',
+        },
         {
             id: 'hearts-6',
             name: 'Hearts 6',
-            image: 'static/img/6_черви.svg',
+            image: './img/6c.png',
         },
         {
             id: 'hearts-7',
             name: 'Hearts 7',
-            image: 'static/img/7_черви.svg',
+            image: './img/7c.png',
         },
+        {
+            id: 'hearts-6',
+            name: 'Hearts 6',
+            image: './img/6c.png',
+        },
+        {
+            id: 'hearts-7',
+            name: 'Hearts 7',
+            image: './img/7c.png',
+        },
+        {
+            id: 'hearts-6',
+            name: 'Hearts 6',
+            image: './img/6c.png',
+        },
+        {
+            id: 'hearts-7',
+            name: 'Hearts 7',
+            image: './img/7c.png',
+        },
+        {
+            id: 'hearts-6',
+            name: 'Hearts 6',
+            image: './img/6c.png',
+        },
+        {
+            id: 'hearts-7',
+            name: 'Hearts 7',
+            image: './img/7c.png',
+        },
+        {
+            id: 'hearts-7',
+            name: 'Hearts 7',
+            image: './img/7c.png',
+        },
+
+        {
+            id: 'diamonds-6',
+            name: 'diamonds 6',
+            image: './img/6b.png',
+        },
+        {
+            id: 'diamonds-7',
+            name: 'diamonds 7',
+            image: './img/7b.png',
+        },
+        {
+            id: 'diamonds-6',
+            name: 'diamonds 6',
+            image: './img/6b.png',
+        },
+        {
+            id: 'diamonds-7',
+            name: 'diamonds 7',
+            image: './img/7b.png',
+        },
+        {
+            id: 'diamonds-6',
+            name: 'diamonds 6',
+            image: './img/6b.png',
+        },
+        {
+            id: 'diamonds-7',
+            name: 'diamonds 7',
+            image: './img/7b.png',
+        },
+        {
+            id: 'diamonds-6',
+            name: 'diamonds 6',
+            image: './img/6b.png',
+        },
+        {
+            id: 'diamonds-7',
+            name: 'diamonds 7',
+            image: './img/7b.png',
+        },
+        {
+            id: 'diamonds-7',
+            name: 'diamonds 7',
+            image: './img/7b.png',
+        },
+        {
+            id: 'clubs-6',
+            name: 'clubs 6',
+            image: './img/6k.png',
+        },
+        {
+            id: 'clubs-7',
+            name: 'clubs 7',
+            image: './img/7k.png',
+        },
+        {
+            id: 'clubs-6',
+            name: 'clubs 6',
+            image: './img/6k.png',
+        },
+        {
+            id: 'clubs-7',
+            name: 'clubs 7',
+            image: './img/7k.png',
+        },
+        {
+            id: 'clubs-6',
+            name: 'clubs 6',
+            image: './img/6k.png',
+        },
+        {
+            id: 'clubs-7',
+            name: 'clubs 7',
+            image: './img/7k.png',
+        },
+        {
+            id: 'clubs-6',
+            name: 'clubs 6',
+            image: './img/6k.png',
+        },
+        {
+            id: 'clubs-7',
+            name: 'clubs 7',
+            image: './img/7k.png',
+        },
+        {
+            id: 'clubs-7',
+            name: 'clubs 7',
+            image: './img/7k.png',
+        }
+
+
+
+
+
     ]
 
     cardListData.forEach((card) => {
@@ -26,22 +217,20 @@ function renderPlayBlock(container) {
         document.body.appendChild(cardElement)
     })
 
-    container.appendChild(resetButton)
+    container.appendChild(cardListData);
+    
 }
 
-window.application.blocks['play'] = renderPlayBlock
+window.application.blocks['cardList'] = renderCardListBlock
 
 function renderPlayScreen() {
-    const app = document.querySelector('.app')
-    const h1 = document.createElement('h1')
-    h1.textContent = '00:00'
-    h1.classList.add('timer')
 
-    const cardField = document.createElement('div')
-    window.application.renderBlock('play', cardField)
+    window.application.renderBlock('play', div);
+    window.application.renderBlock('cardList', div);
 
     app.appendChild(h1)
     app.appendChild(cardField)
+
 }
 
 window.application.screens['play'] = renderPlayScreen
