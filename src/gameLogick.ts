4945552c883160c12ddcf0b9b3aa9eac5b0c5628
Maoleshko <ranshe.rassveta@gmail.com>
2023-04-06 20:53:42 +0300

    final

--- src/gameLogick.ts
+++ src/gameLogick.ts
@@ -8,43 +8,48 @@ export function gameLogick(): void {
     setTimeout(() => {
         cards.forEach((card) => {
             card.classList.remove('flipped')
+
+            cards.forEach((card, index) =>
+                card.addEventListener('click', (e: Event) => {
+                    card.classList.add('flipped')
+                    checkCards(cards[index])
+                })
+            )
+            const checkCards = (e: Element) => {
+                const clickedCard = e
+                if (clickedCard !== null) {
+                    clickedCard.classList.add('flip')
+                }
+                const flippedCards = document.querySelectorAll('.flip')
+
+                if (flippedCards.length === 2) {
+                    if (
+                        flippedCards[0].getAttribute('name') ===
+                        flippedCards[1].getAttribute('name')
+                    ) {
+                        couple++
+                        flippedCards.forEach((card) => {
+                            card.classList.remove('flip')
+                        })
+
+                        if (cards.length === couple * 2) {
+                            setTimeout(() => {
+                                renderEndScreen(
+                                    'Вы победили!',
+                                    "url('./static/img/win.png')"
+                                )
+                            }, 1000)
+                        }
+                    } else {
+                        setTimeout(() => {
+                            renderEndScreen(
+                                'Вы проиграли!',
+                                "url('./static/img/lose.png')"
+                            )
+                        }, 1000)
+                    }
+                }
+            }
         })
     }, 5000)
-
-    cards.forEach((card, index) =>
-        card.addEventListener('click', (e: Event) => {
-            card.classList.add('flipped')
-            checkCards(cards[index])
-        })
-    )
-    const checkCards = (e: Element) => {
-        const clickedCard = e
-        if (clickedCard !== null) {
-            clickedCard.classList.add('flip')
-        }
-        const flippedCards = document.querySelectorAll('.flip')
-
-        if (flippedCards.length === 2) {
-            if (
-                flippedCards[0].getAttribute('name') ===
-                flippedCards[1].getAttribute('name')
-            ) {
-                couple++
-                flippedCards.forEach((card) => {
-                    card.classList.remove('flip')
-                })
-
-                if (cards.length === couple * 2) {
-                    setTimeout(() => {
-                        renderEndScreen('Вы победили!',"url('./static/img/win.png')")
-                    }, 1000)
-                }
-            } else {
-                setTimeout(() => {
-                    renderEndScreen('Вы проиграли!', "url('./static/img/lose.png')")
-                }, 1000)
-                
-            }
-        }
-    }
 }
